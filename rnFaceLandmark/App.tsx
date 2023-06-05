1bd308f77d8368982a6df98fbe4c166e6cdb1dbc
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-05 05:16:23 -0400

    setting up dependencies

--- rnFaceLandmark/App.tsx
+++ rnFaceLandmark/App.tsx
@@ -1,2 +1,22 @@
-import { useState } from 'react';
-import { View, Text } from 'react-native';
+import { useState, useRef, useEffect } from 'react';
+import { Dimensions, StyleSheet, LogBox, Platform, View, Text } from 'react-native';
+import Canvas from 'react-native-canvas';
+import * as tf from '@tensorflow/tfjs';
+import { cameraWithTensors } from '@tensorflow/tfjs-react-native';
+import * as facemesh from '@tensorflow-models/face-landmarks-detection';
+import { Camera } from 'expo-camera';
+
+const TensorCamera = cameraWithTensors(Camera);
+
+const { width, height } = Dimensions.get("window");
+
+
+const App = () => {
+  return (
+    <View>
+      <Text> Main Screen </Text>
+    </View>
+  )
+}
+
+export default App;

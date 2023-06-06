fea96d3af389a72501a3b870fa1e0699e78e118b
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-06 04:31:18 -0400

    implement skia on top of vision camera

--- RNFaceLandmark/App.tsx
+++ RNFaceLandmark/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'react-native-reanimated';
 import { Platform, StyleSheet, useWindowDimensions, View } from 'react-native';
 import {
@@ -9,31 +9,57 @@ import {
 } from 'react-native-vision-camera';
 import { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
 import Animated from 'react-native-reanimated';
+import { Canvas, Image, Circle, useCanvasRef } from '@shopify/react-native-skia';
+import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+// import { drawMesh } from './src/drawMesh';
 
-export function objectDetect(frame : Frame) {
+const inputResolution = {
+  width: 1280,
+  height: 720,
+};
+const videoConstraints = {
+  width: inputResolution.width,
+  height: inputResolution.height,
+};
+
+const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
+const detectorConfig: faceLandmarksDetection.MediaPipeFaceMeshTfjsModelConfig = {
+  runtime: 'tfjs', // or 'tfjs',
+  refineLandmarks: false
+};
+
+
+export function objectDetect(frame: Frame) {
   'worklet';
-  
+
 }
 
 function App() {
+  const canvasRef = useCanvasRef();
+  const [detector, setDetector] = useState<faceLandmarksDetection.FaceLandmarksDetector>();
   // const flag = useSharedValue({ height: 0, left: 0, top: 0, width: 0 });
-  const flag = useSharedValue(false);
 
-  const flagOverlayStyle = useAnimatedStyle(() => { 
-    return {
-      backgroundColor: flag.value ? 'blue' : 'red',
-      position: 'absolute',
-      height: 200,
-      width: 200
-      // ...flag.value,
-    }
-  },
-    [flag],
-  );
+
+  // const handleFrameLoad = (frame: Frame) => {
+  //   'worklet';
+  //   if (canvasRef.current) {
+  //     const imageElement = new Image(canvasRef.current, inputResolution.width, inputResolution.height);
+  //     imageElement.crossOrigin = "anonymous";
+  //   }
+  //   const detect = async (net: faceLandmarksDetection.FaceLandmarksDetector | undefined) => {
+  //     // const estimationConfig = { flipHorizontal: false };
+  //     const faces = await net?.estimateFaces(frame).catch(e => console.error(e));
+
+  //     // console.log(faces[0])
+  //     requestAnimationFrame(() => faces && drawMesh(faces[0], canvasRef.current!));
+  //     detect(detector);
+  //   };
+  //   detect(detector);
+  // }
 
   const frameProcessor = useFrameProcessor(frame => {
     'worklet';
-    
+    // handleFrameLoad(frame);
   }, []);
 
   const devices = useCameraDevices();
@@ -45,6 +71,28 @@ function App() {
     };
     checkPermissions();
   }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      // you can pass an optional rectangle
+      // to only save part of the image
+      const image = canvasRef.current?.makeImageSnapshot();
+      if (image) {
+        // you can use image in an <Image> component
+        // Or save to file using encodeToBytes -> Uint8Array
+        const bytes = image.encodeToBytes();
+      }
+    }, 1000)
+  });
+  // useEffect(() => {
+  //   const checkPermissions = async () => {
+  //     const detector = await faceLandmarksDetection.createDetector(
+  //       model,
+  //       detectorConfig
+  //     );
+  //     setDetector(detector);
+  //   }
+  // }, []);
 
   if (device == null) {
     return null;
@@ -60,6 +108,20 @@ function App() {
         isActive={true}
         orientation="portrait"
       />
+
+
+      {/* // style={{ */}
+      {/* //   position: "absolute",
+      //   marginLeft: "auto",
+      //   marginRight: "auto",
+      //   left: 0,
+      //   right: 0,
+      //   width: inputResolution.width,
+      //   height: inputResolution.height */}
+      {/* // }} */}
+      <Canvas style={{ flex: 1 }} ref={canvasRef}>
+        <Circle r={128} cx={128} cy={128} color="red" />
+      </Canvas>
     </>
   );
 }
--- RNFaceLandmark/src/drawMesh.tsx
+++ RNFaceLandmark/src/drawMesh.tsx
@@ -1,6 +1,7 @@
 import { TRIANGULATION } from "./TRIANGULATION";
+import Canvas, { CanvasRenderingContext2D ,Path2D } from 'react-native-canvas'
 
-export const drawMesh = (prediction, ctx) => {
+export const drawMesh = (prediction : any, can : Canvas) => {
     try {
         if (!prediction) {
             console.log("error")
@@ -9,14 +10,15 @@ export const drawMesh = (prediction, ctx) => {
         const keyPoints = prediction.keypoints;
 
         if (!keyPoints) return; // do not draw if there is no keypoints
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); //clear the canvas after every drawing
+        const ctx = can.getContext("2d");
+        ctx.clearRect(0, 0, can.width, can.height); //clear the canvas after every drawing
         for (let i = 0; i < TRIANGULATION.length / 3; i++) {
             const points = [
                 TRIANGULATION[i * 3],
                 TRIANGULATION[i * 3 + 1],
                 TRIANGULATION[i * 3 + 2],
             ].map((index) => keyPoints[index]);
-            drawPath(ctx, points, true);
+            drawPath(can, points, true);
         }
 
         for (let keyPoint of keyPoints) {
@@ -31,14 +33,15 @@ export const drawMesh = (prediction, ctx) => {
     }
 }
 
-const drawPath = (ctx, points, closePath) => {
-    const region = new Path2D();
+const drawPath = (can : Canvas, points: any[], closePath : boolean) => {
+    const ctx = can.getContext("2d");
+    const region = new Path2D(can);
     region.moveTo(points[0].x, points[0].y);
     for (let i = 1; i < points.length; i++) {
         const point = points[i];
         region.lineTo(point.x, point.y);
     }
     if (closePath) region.closePath();
-    ctx.stokeStyle = "black";
+    ctx.strokeStyle = "black";
     ctx.stroke(region);
 };
